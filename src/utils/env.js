c59708a21570c7a1eef979b6e70091af467e2fde
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-02-23 09:15:07 +0700

    feat(source): source code

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = "https://lyv.lacty.com.vn";
 // export const BASE_URL = "http://192.168.32.82:8080";
-export const BASE_URL = "https://03c462c37c32e2707affd9000029aacd.serveo.net";
+export const BASE_URL = "https://81d5a08547f0a9dde25885fc16c937c5.serveo.net";
