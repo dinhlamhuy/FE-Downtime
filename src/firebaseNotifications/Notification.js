2a9df86be19b0cb9e5cc7848a7a65b3d03ab5f3b
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-02-28 09:26:23 +0700

    check2

--- src/firebaseNotifications/Notification.js
+++ src/firebaseNotifications/Notification.js
@@ -3,39 +3,45 @@ import toast, { Toaster } from "react-hot-toast";
 import { requestForToken, onMessageListener } from "./firebase";
 
 const Notification = () => {
-    
-    const [notification, setNotification] = useState({ title: "", body: "" });
+  const [notification, setNotification] = useState({ title: "", body: "" });
 
-    const notify = () => toast(<ToastDisplay />);
+  const notify = () => toast(<ToastDisplay />);
 
-    function ToastDisplay() {
-        return (
-            <div>
-                <b>{notification?.title}</b>
-                <p>{notification?.body}</p>
-            </div>
-        );
+  function ToastDisplay() {
+    return (
+      // <div>
+
+      //     <b>{notification?.title}</b>
+      //     <p>{notification?.body}</p>
+      // </div>
+
+      <div>
+        <a href="http://localhost:3000/electric">
+          <b>{notification?.title}</b>
+          <p>{notification?.body}</p>
+        </a>
+      </div>
+    );
+  }
+
+  useEffect(() => {
+    if (notification?.title) {
+      notify();
     }
+  }, [notification]);
 
-    useEffect(() => {
-        if (notification?.title) {
-            notify();
-        }
-    }, [notification]);
+  requestForToken();
 
-    requestForToken();
+  onMessageListener()
+    .then((payload) => {
+      setNotification({
+        title: payload?.notification?.title,
+        body: payload?.notification?.body,
+      });
+    })
+    .catch((err) => console.log("failed: ", err));
 
-    onMessageListener()
-        .then((payload) => {
-            setNotification({
-                title: payload?.notification?.title,
-                body: payload?.notification?.body,
-            });
-        })
-        .catch((err) => console.log("failed: ", err));
-
-    return <Toaster />;
+  return <Toaster />;
 };
 
 export default Notification;
-
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "https://192.168.18.172:8080";
 
 // export const BASE_URL = "https://lyv.lacty.com.vn";
-export const BASE_URL = "http://192.168.32.82:8080";
-// export const BASE_URL = "https://2aa4ccdf68b5a9edcd24d4d7587d8edc.serveo.net";
+// export const BASE_URL = "http://192.168.32.82:8080";
+export const BASE_URL = "https://847adfd5c82326201717cf1aa1885ef1.serveo.net";
