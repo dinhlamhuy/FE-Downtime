d64afdf508f0ba45af69a41af8b66dd574bf8212
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-03-01 07:57:50 +0700

    check3

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = "https://lyv.lacty.com.vn";
 // export const BASE_URL = "http://192.168.32.82:8080";
-export const BASE_URL = "https://8acae71e90c305b7b242a09f441b8b81.serveo.net";
+export const BASE_URL = "https://grumpy-mice-mate.loca.lt";
