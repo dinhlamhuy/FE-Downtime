2341d0446a7f251b15deb061a88afb32b06ac5b7
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-06-06 15:57:40 +0700

    upload 2

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = "https://lyv.lacty.com.vn";
 // export const BASE_URL = "http://192.168.32.169:8080";
-export const BASE_URL = "https://2ae616bcaf4be67370e9f6aea0357ac0.serveo.net";
+export const BASE_URL = "https://4ae399be136261d8a577b446645cd42e.serveo.net";
