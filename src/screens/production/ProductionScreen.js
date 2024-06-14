9d606233b49c2e87a1b6b6146e069421b0b3f826
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-06-14 10:05:02 +0700

    upl

--- src/screens/production/ProductionScreen.js
+++ src/screens/production/ProductionScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginScreen from "../LoginScreen";
 import SideBar from "../../components/SideBar";
 import RoutesProdution from "../../config/RoutesProdution";
@@ -9,33 +9,47 @@ import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 const ProductionScreen = () => {
-    const auth = useSelector((state) => state.auth);
-    const [t] = useTranslation("global");
+  const auth = useSelector((state) => state.auth);
+  const [t] = useTranslation("global");
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const handleSidebarToggle = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
 
-    const sideBarMenu = [
-        {
-            icon: <NotificationsNoneIcon />,
-            text: t("sidebar.info_machine_damage"),
-            path: "/product",
-        },
-        {
-            icon: <AutorenewIcon />,
-            text: t("sidebar.process_status"),
-            path: "/product/status",
-        },
-    ];
+  const handleSidebarClose = () => {
+    setSidebarOpen(false);
+  };
 
-    return (
-        <React.Fragment>
-            {auth.user !== null && auth.user?.permission === 3 ? (
-                <SideBar sideBarMenu={sideBarMenu} user={auth.user}>
-                    <RoutesProdution />
-                </SideBar>
-            ) : (
-                <LoginScreen />
-            )}
-        </React.Fragment>
-    );
+  const sideBarMenu = [
+    {
+      icon: <NotificationsNoneIcon />,
+      text: t("sidebar.info_machine_damage"),
+      path: "/product",
+    },
+    {
+      icon: <AutorenewIcon />,
+      text: t("sidebar.process_status"),
+      path: "/product/status",
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      {auth.user !== null && auth.user?.permission === 3 ? (
+        <SideBar
+          sideBarMenu={sideBarMenu}
+          user={auth.user}
+          open={isSidebarOpen}
+          onToggle={handleSidebarToggle}
+          onClose={handleSidebarClose}
+        >
+          <RoutesProdution />
+        </SideBar>
+      ) : (
+        <LoginScreen />
+      )}
+    </React.Fragment>
+  );
 };
 
 export default ProductionScreen;
