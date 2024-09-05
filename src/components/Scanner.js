797455d6f32bd76b859dbd30d4247edd2fb34c35
dinhlamhuy <dinhlamhuytak489@gmail.com>
2024-09-05 07:37:26 +0700

    gd

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -12,8 +12,8 @@ const Scanner = (props) => {
   useEffect(() => {
     const scanner = new Html5QrcodeScanner(`render-${idMachine}`, {
       qrbox: {
-        width: 150,  // Kích thước nhỏ hơn để tập trung vào mã QR nhỏ
-        height: 150,
+        width: 120,  // Kích thước nhỏ hơn để tập trung vào mã QR nhỏ
+        height: 120,
       },
       fps: 15,
       videoConstraints: {
